4e53a128169b932fb8b7f84e392f00fddd4bbb52
Javier gonzalez <fjarmas91@gmail.com>
2025-02-06 16:47:21 -0600

    end of app

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const Bands = require('./models/bands');
+const Band = require('./models/band');
 require('dotenv').config();
 
 //App express
@@ -7,9 +9,48 @@ const app = express();
 
 //Node server
 const server = require('http').createServer(app);
-module.exports.io = require('socket.io')(server);
+const io = require('socket.io')(server);
 
-require('./sockets/socket');
+//
+const bands = new Bands;
+
+//
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Foo Fighter'));
+bands.addBand(new Band('Heroes'));
+bands.addBand(new Band('Salsa'));
+
+//Mensajes de Sockets
+io.on('connection', client => {
+    console.log("cliente conectado");
+
+    client.emit('active-bands', bands.getBands());
+
+    client.on('disconnect', () => { 
+        console.log('cliente desconectado') });
+
+    client.on('mensaje',(payload)=>{
+        console.log('aceptar mensaje', payload);
+        io.emit('mensaje', {admin:'nuevo mensaje'});
+    });
+
+    client.on('vote-band', (payload)=> {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => { 
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBands(payload.id);
+        io.emit('active-bands', bands.getBands());
+    })
+
+  });
 
 const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,18 @@
-const io = require('../index');
+var io = require('../index.js');
+const Bands = require('../models/bands.js');
+const Band = require('../models/band.js');
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Foo Fighter'));
+bands.addBand(new Band('Heroes'));
+bands.addBand(new Band('Salsa'));
+
 //Mensajes de Sockets
 io.on('connection', client => {
     console.log("cliente conectado");
+    
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => { 
         console.log('cliente desconectado') });
